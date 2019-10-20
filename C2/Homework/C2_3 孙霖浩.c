d788a9c426b2aec327d8f94a38d479aec5152dab
ITFS777 <1374924410@qq.com>
2019-10-20 11:47:36 +0800

    2019.10.20

--- C2/Homework/C2_3 孙霖浩.c
+++ C2/Homework/C2_3 孙霖浩.c
@@ -1,7 +1,7 @@
 int mystrcmp(const char *s1, const char *s2)
 {
     int i = 0;
-    for (i = 0; (s1[i] != '\0') && (s2[i] != '\0'); i++)
+    for (i = 0; (s1[i] != '\0') || (s2[i] != '\0'); i++)
     {
         if (s1[i] != s2[i])
         {
