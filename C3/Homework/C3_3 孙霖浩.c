b09dc34c7bdfbafb606b5f527bed90ab6b1db275
ITFS777 <1374924410@qq.com>
2019-11-17 00:12:54 +0800

    C3_3

--- C3/Homework/C3_3 孙霖浩.c
+++ C3/Homework/C3_3 孙霖浩.c
@@ -1,136 +1,126 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <windows.h>
 #include <stdbool.h>
+#include <math.h>
 #define NOTEST
+#define MAX_VALUE_COUNT 1000
+#define MAX_VAR_NAME_LENGTH 128
+#define MAX_DIG_LENGTH 128
+#define MAX_SYMBOL_COUNT 128
+#define HASH_SEED "by_ITFS"
 //////////////////////////////////////////////////////////////////////////
-typedef struct list
-{
-    char *var_name;
-    double value;
-    struct list *last;
-    struct list *next;
-} var;
-//////////////////////////////////////////////////////////////////////////
-struct list *add_var(struct list *p);
-void del_var(struct list *p);
-struct list *search_var(struct list *p, char *var_name);
-void print_allvar(struct list *p);
-void del_allvar(struct list *p);
-char *get_string(char *str);
-char *get_name(char *str);
-int get_type(char ch);
-void skip_space();
+bool is_letter(char ch);
+bool is_num(char ch);
+bool is_symbol(char ch);
+double *hash(const char *name, const double *base, int size, const char *hash_seed);
+double com(double **value_list, int value_count, char *symbol_list, int symbol_count);
 //////////////////////////////////////////////////////////////////////////
 #ifdef NOTEST
 int main(int argc, char *argv[])
 {
-    char flag = 0, ch = 0;
-
-    int var_count = 0, i = 0;
-
-    double tmp_value = 0.0;
-
-    var *head = NULL;
-    var *pvar = NULL;
-_get_exp:
-    char *exp = (char *)calloc(1, sizeof(char));
-    if (get_string(exp) == NULL) //错误处理
-        goto _error;
-
-_judge_type:
-
-    skip_space(exp, &i);
-    switch (get_type(exp, &i)) //判断当前项类型
+    /*-----------------------------------声明变量-----------------------------------*/
+    int i = 0, j = 0, length = 0;
+    char ch = 0;
+    //表达式值相关声明
+    int value_count = 0;
+    int digit_count = 0;
+    double result = 0.0;
+    double *var = NULL;
+    double digit_list[MAX_VALUE_COUNT] = {0};
+    double *value_list[MAX_VALUE_COUNT] = {0};
+    char digit_string[MAX_DIG_LENGTH] = {0};
+    char var_name[MAX_VAR_NAME_LENGTH] = {0};
+    //表达式符号相关声明
+    int symbol_count = 0;
+    char symbol_list[MAX_SYMBOL_COUNT] = {0};
+    //定义变量哈希表
+    double * var = (double *)calloc(MAX_VALUE_COUNT, sizeof(double));
+    while (1)
     {
-    case 0: //非法输入
-
-        printf("error input!\n");
-        fflush(NULL);
-        goto _get_exp;
-
-    case 1: //变量
-
-        //临时存储变量名
-        char *tmp_varname = (char *)calloc(1, sizeof(char));
-        if (get_name(exp, tmp_varname, &i) == NULL) //错误处理
-            goto _error;
-
-        if (search_var(head, tmp_varname) == NULL) //声明变量
+        /*----------------------------------初始化变量----------------------------------*/
+        i = 0;
+        j = 0;
+        length = 0;
+        ch = 0;
+        value_count = 0;
+        digit_count = 0;
+        symbol_count = 0;
+        result = 0;
+        memset(digit_list, 0, MAX_VALUE_COUNT * sizeof(double));
+        memset(value_list, 0, MAX_VALUE_COUNT * sizeof(double *));
+        memset(digit_string, 0, MAX_DIG_LENGTH * sizeof(char));
+        memset(var_name, 0, MAX_VAR_NAME_LENGTH * sizeof(char));
+        memset(symbol_list, 0, MAX_SYMBOL_COUNT * sizeof(char));
+        /*----------------------------------读取表达式----------------------------------*/
+        char *exp = (char *)calloc(1, sizeof(char));
+        for (i = 0; (ch = getchar()) != '\n'; /*void*/)
         {
-            if (var_count == 0)
+            exp[i] = ch;
+            i++;
+            exp = realloc(exp, (i + 1) * sizeof(char));
+            if (exp == NULL)
+                return 0;
+            exp[i] = '\0';
+        }
+        length = i; //记录长度
+        /*----------------------------------处理表达式----------------------------------*/
+        for (i = 0, value_count = 0, symbol_count = 0, digit_count = 0; i < length; i++)
+        {
+            if (is_letter(exp[i])) //检测到变量
             {
-                head = add_var(head);
-                pvar = head;
+                memset(var_name, 0, MAX_VAR_NAME_LENGTH * sizeof(char)); //初始化
+                j = 0;
+                while (is_letter(exp[i]) || is_num(exp[i]))
+                {
+                    var_name[j] = exp[i];
+                    j++;
+                    i++;
+                }
+                value_list[value_count] = hash(var_name, var, MAX_SYMBOL_COUNT, HASH_SEED); //将变量名映射到哈希地址
+                value_count++;
+                i--;
             }
-            else
-                pvar = add_var(pvar);
-            //将临时变量名存储在链表中
-            pvar->var_name = (char *)calloc(strlen(tmp_varname) + 1, sizeof(char));
-            strncpy(pvar->var_name, tmp_varname, strlen(tmp_varname) + 1);
-            var_count++;
+            else if (is_symbol(exp[i])) //检测到符号
+            {
+                symbol_list[symbol_count] = exp[i];
+                symbol_count++;
+            }
+            else if (is_num(exp[i])) //检测到数值
+            {
+                memset(digit_string, 0, MAX_DIG_LENGTH * sizeof(char)); //初始化
+                j = 0;
+                while (is_num(exp[i])) //将数值保存为字符串
+                {
+                    digit_string[j] = exp[i];
+                    j++;
+                    i++;
+                }
+                digit_list[digit_count] = atof(digit_string);       //转化为字符
+                value_list[value_count] = &digit_list[digit_count]; //储存在值序列中
+                digit_count++;
+                value_count++;
+                i--;
+            }
         }
-        free(tmp_varname);
-        tmp_value = pvar->var_value; //暂存表达式的值
-        goto _judge_type;
+        free(exp); //释放内存
+        /*for( i = 0; i < symbol_count; i++)
+            printf("%c ",symbol_list[i]);*/
+        /*----------------------------------计算表达式----------------------------------*/
 
-    case 2: //数值
-
-        scanf(" %lf", &tmp_value);
-        goto _judge_type;
-
-    case 3: //运算
-
-        tmp_value = opera(tmp_value, exp, &i, head);
-        goto _judge_type;
-
-    case 4: //赋值
-
-        search_var(head, tmp_varname)->value = opera(tmp_value, exp, &i, head);
-        goto _get_exp;
-
-    case 5: //命令
-
-    case 6: //结束
-        break;
-    default:
-        goto _error;
-    }
-    printf("%lf\n", tmp_value);
-_var:
-
-    //判断变量操作类型
-    switch (get_type(exp, &i))
-    {
-    case 0: //非法输入
-        printf("error input!\n");
-        goto _judge_type;
-    case 4: //赋值
-        skip_space();
-
-        else pvar = search_var(head, tmp_varname); //查找变量
-
-        pvar->value = get_value(); //变量赋值
-        break;
-    case 5:                                   //打印变量值
-        pvar = search_var(head, tmp_varname); //查找变量
-        if (pvar == NULL)                     //错误处理
+        if (symbol_list[0] == '=') //赋值表达式
         {
-            printf("unknown var!\n");
-            goto _judge_type;
+            *value_list[0] = com(value_list + 1, value_count - 1, symbol_list + 1, symbol_count - 1);
+            *hash("_", var, MAX_VALUE_COUNT, HASH_SEED) = *value_list[0];
         }
-        else //打印变量
-            printf("%lf\n", pvar->value);
-        break;
-    default:
-        goto _error;
+        else //运算表达式
+        {
+            result = com(value_list, value_count, symbol_list, symbol_count);
+            *hash("_", var, MAX_VALUE_COUNT, HASH_SEED) = result;
+            printf("%lf\n", result); //输出运算结果
+        }
+        /*-----------------------------------计算结束----------------------------------*/
     }
-
-    return 0;
-_error:
-    printf("unknown error!\n");
-    system("pause");
     return 0;
 }
 #endif
@@ -143,237 +133,75 @@ int main(int argc, char *argv[])
 }
 #endif
 //////////////////////////////////////////////////////////////////////////
-
-//////////////////////////////////////////////////////////////////////////
-struct list *add_var(struct list *p)
+bool is_letter(char ch)
 {
-    struct list *list = (struct list *)calloc(1, sizeof(struct list));
-    list->value = 0.0;
-    list->last = p;
-    list->next = NULL;
-    return list;
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch == '_'))
+        return true;
+    else
+        return false;
 }
 //////////////////////////////////////////////////////////////////////////
-void del_var(struct list *p)
+bool is_num(char ch)
 {
-    p->last->next = p->next;
-    p->next->last = p->last;
-    free(p);
+    if ((ch >= '0' && ch <= '9') || (ch == '.'))
+        return true;
+    else
+        return false;
 }
 //////////////////////////////////////////////////////////////////////////
-struct list *search_var(struct list *p, char *var_name)
+bool is_symbol(char ch)
 {
-    do
-    {
-        if (strcmp(var_name, p->var_name) == 0)
-            return p;
-        else
-            p = p->next;
-    } while (p != NULL);
-    return NULL;
+    if ((ch == '+') || (ch == '-') || (ch == '*') || (ch == '/') || (ch == '^') || (ch == '='))
+        return true;
+    else
+        return false;
 }
 //////////////////////////////////////////////////////////////////////////
-void print_allvar(struct list *p)
-{
-    do
-    {
-        printf("%s : %lf\n", p->var_name, p->value);
-        p = p->next;
-    } while (p != NULL);
-}
-//////////////////////////////////////////////////////////////////////////
-void del_allvar(struct list *p)
-{
-    while (p->next != NULL)
-    {
-        p = p->next;
-        free(p->last);
-    }
-    free(p);
-}
-//////////////////////////////////////////////////////////////////////////
-char *get_string(char *str)
+double *hash(const char *name, const double *base, int size, const char *hash_seed)
 {
     int i = 0;
-    char ch = 0;
-    for (i = 0; (ch = getchar()) != '\n'; /*void*/)
+    long long int tmp = 0, p3 = 0;
+    char p1 = 0, p2 = 0;
+    for (i = 0; i < strlen(name); i++)
     {
-        str[i] = ch;
-        i++;
-        str = realloc(str, (i + 1) * sizeof(char));
-        if (str == NULL)
-            return NULL;
-        str[i] = '\0';
+        p1 = (name[i] & 0xA5) | (hash_seed[i % strlen(hash_seed)] & 0x5A);
+        p2 = (name[i] & 0x5A) | (hash_seed[i % strlen(hash_seed)] & 0xA5);
+        tmp += ((p1 & 0xF0) | (p2 & 0x0F)) + ((p1 & 0x0F) | (p2 & 0xF0));
+        p3 += hash_seed[i % strlen(hash_seed)];
     }
-    return str;
+    return (base + (tmp*p3 % size));
 }
 //////////////////////////////////////////////////////////////////////////
-char *get_name(char *exp, char *str, int *i)
+double com(double **value_list, int value_count, char *symbol_list, int symbol_count)
 {
-    int j = 0;
-    while (exp[*i] != ' ')
+    int i = 0, j = 0;
+    double result = 0;
+    result = *value_list[0];
+    for (i = 1, j = 0; (i < value_count) && (j < symbol_count); i++, j++)
     {
-        str[j] = exp[*i];
-        (*i)++;
-        j++;
-        str = realloc(str, (j + 1) * sizeof(char));
-        if (str == NULL)
-            return NULL;
-        str[j] = '\0';
-    }
-    return str;
-}
-//////////////////////////////////////////////////////////////////////////
-int get_type(char *exp, int *i)
-{
-    if ((exp[*i] >= 'a' && exp[*i] <= 'z') || (exp[*i] >= 'A' && exp[*i] <= 'Z') || (exp[*i] == '_') || (exp[*i] == '$'))
-        return 1; //变量
-    else if (exp[*i] >= '0' && exp[*i] <= '9' || exp[*i] == '.')
-        return 2; //数值
-    else if ((exp[*i] == '+') || (exp[*i] == '-') || (exp[*i] == '*') || (exp[*i] == '/'))
-        return 3; //运算
-    else if (exp[*i] == '=')
-    {
-        (*i)++;
-        return 4; //赋值
-    }
-    else if (exp[*i] == '#')
-    {
-        (*i)++;
-        return 5; //命令
-    }
-    else if (exp[*i] == '\n')
-        return 6; //换行
-    else
-        return 0; //非法输入
-}
-//////////////////////////////////////////////////////////////////////////
-void skip_space(char *exp, int *i)
-{
-    while (exp[*i] == ' ')
-        (*i)++;
-}
-//////////////////////////////////////////////////////////////////////////
-double opera(double value, char *exp, int *i, struct list *head)
-{
-    skip_space(exp, i);
-    switch (exp[*i])
-    {
-    case '+':
-        (*i)++;
-        skip_space(exp, i);
-        switch (get_type(exp[*i]))
+        switch (symbol_list[j])
         {
-        case 1:
-            //临时存储变量名
-            char *tmp_varname = (char *)calloc(1, sizeof(char));
-            if (get_name(exp, tmp_varname, i) == NULL) //错误处理
-                goto _error;
-            value += search_var(head, tmp_varname)->value;
-            free(tmp_varname);
+        case '+':
+            result += *value_list[i];
             break;
-        case 2:
-            value += strtod(exp, &exp); //读取字符串中的浮点数
-            *i = 0;
+        case '-':
+            result -= *value_list[i];
             break;
-        case 6:
-            return value; //判断表达式结尾
-        case 0:
-        default:
-            printf("error input!\n");
-            fflush(NULL);
-            return value;
+        case '*':
+            result *= *value_list[i];
+            break;
+        case '/':
+            if (value_list[i] == 0)
+                return 0.0;
+            result /= *value_list[i];
+            break;
+        case '^':
+            if ((result < 0) && (value_list[i] < 0))
+                return 0.0;
+            result = pow(result, *value_list[i]);
+            break;
         }
-        return opera(value, exp, i, head); //嵌套调用，往后计算表达式
-    case '-':
-        (*i)++;
-        skip_space(exp, &i);
-        switch (get_type(exp[i]))
-        {
-        case 1:
-            //临时存储变量名
-            char *tmp_varname = (char *)calloc(1, sizeof(char));
-            if (get_name(exp, tmp_varname, &i) == NULL) //错误处理
-                goto _error;
-            value -= search_var(head, tmp_varname)->value;
-            free(tmp_varname);
-            break;
-        case 2:
-            value -= strtod(exp, &exp); //读取字符串中的浮点数
-            *i = 0;
-            break;
-        case 6:
-            return value;
-        case 0:
-        default:
-            printf("error input!\n");
-            fflush(NULL);
-            return value;
-        }
-        return opera(value, exp, i, head);
-    case '*':
-        (*i)++;
-        skip_space(exp, &i);
-        switch (get_type(exp[i]))
-        {
-        case 1:
-            //临时存储变量名
-            char *tmp_varname = (char *)calloc(1, sizeof(char));
-            if (get_name(exp, tmp_varname, &i) == NULL) //错误处理
-                goto _error;
-            value *= search_var(head, tmp_varname)->value;
-            free(tmp_varname);
-            break;
-        case 2:
-            value *= strtod(exp, &exp); //读取字符串中的浮点数
-            *i = 0;
-            break;
-        case 6:
-            return value;
-        case 0:
-        default:
-            printf("error input!\n");
-            fflush(NULL);
-            return value;
-        }
-        return opera(value, exp, i, head);
-    case '/':
-        (*i)++;
-        skip_space(exp, &i);
-        switch (get_type(exp[i]))
-        {
-        case 1:
-            //临时存储变量名
-            char *tmp_varname = (char *)calloc(1, sizeof(char));
-            if (get_name(exp, tmp_varname, &i) == NULL) //错误处理
-                goto _error;
-            value /= search_var(head, tmp_varname)->value;
-            free(tmp_varname);
-            break;
-        case 2:
-            value /= strtod(exp, &exp); //读取字符串中的浮点数
-            *i = 0;
-            break;
-        case 6:
-            return value;
-        case 0:
-        default:
-            printf("error input!\n");
-            fflush(NULL);
-            return value;
-        }
-        return opera(value, exp, i, head);
     }
+    return result;
 }
 //////////////////////////////////////////////////////////////////////////
-bool is_newvar(struct list *p, char *var_name)
-{
-    do
-    {
-        if (!strcmp(p->var_name, var_name))
-            return p;
-        else
-            p = p->next;
-    } while (p != NULL);
-    return NULL;
-}
